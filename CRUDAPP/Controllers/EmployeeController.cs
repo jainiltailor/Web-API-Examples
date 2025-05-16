311e579ca5eb5981439fe9d6113f7121c701a210
jainiltailor <jk.tailor1734@gmail.com>
2025-05-16 20:48:25 +0530

    Initial commit of .NET project

---
+++ CRUDAPP/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using CRUDAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace CRUDAPP.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:44385/api");
+        private readonly HttpClient UserClient;
+
+
+        public EmployeeController()
+        {
+            UserClient = new HttpClient();
+
+            UserClient.BaseAddress = baseAddress;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EmployeeViewModel> employees = new List<EmployeeViewModel>();
+            HttpResponseMessage Response = UserClient.GetAsync(UserClient.BaseAddress + "/" + "Employee").Result;
+
+            if (Response.IsSuccessStatusCode)
+            { 
+                string data=Response.Content.ReadAsStringAsync().Result;
+                employees = JsonConvert.DeserializeObject<List<EmployeeViewModel>>(data);
+            }
+
+            return View(employees);
+        }
+       
+
+
+    }
+}
---
+++ CRUDAPP/Models/EmployeeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDAPP.Models
+{
+    public class EmployeeViewModel
+    {
+
+        [Key]
+        public Guid EmpID { get; set; }
+        public required string EmpName { get; set; }
+        public string? EmpEmail { get; set; }
+        public string? Phone { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
